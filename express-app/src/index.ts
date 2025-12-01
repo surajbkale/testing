ede1b9ee137492d307dcb5f3f213aa0408386f56
surajbkale <surajkalecsit@gmail.com>
2025-12-02 01:07:08 +0530

    added the express

---
+++ express-app/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+export const app = express();
+app.use(express.json());
+
+app.post("/sum", (req, res) => {
+  const { a, b } = req.body;
+  const answer = a + b;
+  res.json({
+    answer,
+  });
+});
