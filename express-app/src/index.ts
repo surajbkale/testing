c3a17e45bd8bfe01268e17e25bdb9331edc9471f
surajbkale <surajkalecsit@gmail.com>
2025-12-02 01:18:40 +0530

    learned how to test the headers and how to cover full tests

--- express-app/src/index.ts
+++ express-app/src/index.ts
@@ -1,11 +1,44 @@
 import express from "express";
+import { z } from "zod";
 
 export const app = express();
 app.use(express.json());
 
+const sumInput = z.object({
+  a: z.number(),
+  b: z.number(),
+});
+
 app.post("/sum", (req, res) => {
-  const { a, b } = req.body;
-  const answer = a + b;
+  const parsedData = sumInput.safeParse(req.body);
+
+  if (!parsedData.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+
+  const answer = parsedData.data.a + parsedData.data.b;
+
+  res.json({
+    answer,
+  });
+});
+
+app.get("/sum", (req, res) => {
+  const parsedData = sumInput.safeParse({
+    a: Number(req.headers["a"]),
+    b: Number(req.headers["b"]),
+  });
+
+  if (!parsedData.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+
+  const answer = parsedData.data.a + parsedData.data.b;
+
   res.json({
     answer,
   });
